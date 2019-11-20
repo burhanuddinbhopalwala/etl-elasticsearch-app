89c8c343081cd23c657e50b67f776ff377359d05
Burhanuddin Bhopalwala <burhanuddinbhopalwala.cse@gmail.com>
2019-11-21 01:37:21 +0530

    fix(.gitignore): for .yml to .yaml

--- config/connect.rb
+++ config/connect.rb
@@ -2,13 +2,13 @@
 
 class Connect
   def self.load_db_config
-    #* db_configuration_file = File.join(File.expand_path('..', __FILE__), "..", "db", "database.yml")
-    db_configuration_file = File.join(File.expand_path(__dir__), "database.yml")
+    #* db_configuration_file = File.join(File.expand_path('..', __FILE__), "..", "db", "database.yaml")
+    db_configuration_file = File.join(File.expand_path(__dir__), "database.yaml")
     YAML.load(File.read(db_configuration_file))
   end
 
   def self.load_elasticsearch_config
-    elasticsearch_configuration_file = File.join(File.expand_path(__dir__), "elasticsearch.yml")
+    elasticsearch_configuration_file = File.join(File.expand_path(__dir__), "elasticsearch.yaml")
     YAML.load(File.read(elasticsearch_configuration_file))
   end
 end
